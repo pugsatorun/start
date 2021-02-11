fa703452b2ba277374d90a1b621ab2b6f71ae093
pugsatoshi <satoshi.ito.dev@gmail.com>
2021-02-11 14:18:34 +0900

    pair

--- setting.sh
+++ setting.sh
@@ -8,19 +8,19 @@ sudo apt upgrade -y
 sudo apt install -y curl vim nano openssh-server git 
 
 #日本語環境install
-echo "日本語環境install"
-sudo apt -y install language-pack-ja-base language-pack-ja ibus-mozc
-sudo update-locale LANG=ja_JP.UTF8
-sudo apt -y install manpages-ja manpages-ja-dev
+#echo "日本語環境install"
+#sudo apt -y install language-pack-ja-base language-pack-ja ibus-mozc
+#sudo update-locale LANG=ja_JP.UTF8
+#sudo apt -y install manpages-ja manpages-ja-dev
 #タイムゾーンを日本に変更
-echo "タイムゾーンの設定"
-ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/locltime
+#echo "タイムゾーンの設定"
+#ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/locltime
 
 #vim環境構築
 mkdir ~/.vim
 mkdir ~/.vim/colors
 git clone https://github.com/tomasr/molokai ~/.vim/colors/molokai
-cp ~/.vim/colors/molokai/colors/molokai.vim ~/.vim.colors
+cp ~/.vim/colors/molokai/colors/molokai.vim ~/.vim/colors
 
 #任意のファイルのコピー
 cp .vimrc ~/
