c81d9de8f2da035b7a6369f1ad5a208308837b2a
pugsatoshi <satoshi.ito.dev@gmail.com>
2021-07-04 00:44:39 +0900

    repair

--- ubuntu-setting.sh
+++ ubuntu-setting.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt upgrade -y
 
 #初期導入コマンド
-sudo apt install -y curl vim nano openssh-server git 
+sudo apt install -y curl vim nano openssh-server git build-essential
 
 #git初期設定
 git config --global user.name "name"
@@ -38,3 +38,49 @@ echo "alias cp='cp -i'" | sudo tee -a ~/.bashrc
 echo "alias rm='rm -i'" | sudo tee -a /etc/profile
 echo "alias mv='mv -i'" | sudo tee -a /etc/profile
 echo "alias cp='cp -i'" | sudo tee -a /etc/profile
+
+#Dockerインストール
+sudo apt-get update
+sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+#Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# x_86_64/amd64
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# arm64
+#echo \
+#  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl status docker
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+sudo systemctl restart docker
+
+# pyenv導入
+# pyenv初期コマンド
+sudo apt install -y \
+  libffi-dev \
+  libssl-dev \
+  zlib1g-dev \
+  liblzma-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+source ~/.bashrc
+echo "pyenv導入確認"
+pyenv -v
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
